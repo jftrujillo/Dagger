f584539e6a6001afe1f12f1f51c1445cf3d42b01
jhon fredy <jhon.trujillo@seratic.com>
2017-10-24 14:45:18 -0500

    logica de base de datos inicia

---
+++ app/src/main/java/codemakers/daggermvvm/App.kt
@@ -0,0 +1,10 @@
+package codemakers.daggermvvm
+
+import android.app.Application
+
+/**
+ * Created by Administrador on 24/10/2017.
+ */
+class App : Application(){
+
+}
---
+++ app/src/main/java/codemakers/daggermvvm/data/model/Task.kt
@@ -0,0 +1,18 @@
+package codemakers.daggermvvm.data.model
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+/**
+ * Created by Administrador on 24/10/2017.
+ */
+@Entity
+class Task(@PrimaryKey(autoGenerate = true)
+           @ColumnInfo(name = "id")
+           var id : Int,
+           @ColumnInfo(name = "Titulo")
+           var title : String,
+           @ColumnInfo(name = "descripcion")
+           var descripcion : String
+           )
